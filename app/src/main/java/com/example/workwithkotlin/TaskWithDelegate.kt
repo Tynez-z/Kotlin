426937ff214d8ca0fd3a8f642a0c97c098a6fe76
Tanya <tatyana.kazey1@gmail.com>
2021-04-28 12:09:47 +0300

    Done

--- app/src/main/java/com/example/workwithkotlin/TaskWithDelegate.kt
+++ app/src/main/java/com/example/workwithkotlin/TaskWithDelegate.kt
@@ -9,11 +9,11 @@ class User {
 
 class DelegatedUser {
     operator fun getValue(user: User, property: KProperty<*>): String {
-        return "Get value"
+        return "$user, Get value '${property.name}'"
     }
 
     operator fun setValue(user: User, property: KProperty<*>, value: String) {
-        println("Set value")
+        println("Set value: $value значение '${property.name} у $user'")
     }
 }
 
@@ -21,6 +21,7 @@ fun main(args: Array<String>) {
     var user = User()
     println(user.Task)
     user.Task = "123"
+
     if (::a.isInitialized) {
         println("a is initialized")
     } else {
